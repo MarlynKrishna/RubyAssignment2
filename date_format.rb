1d12950a095bbf295a0ead55906fb9c1ec61305f
MarlynKrishna <88415687+MarlynKrishna@users.noreply.github.com>
2022-07-25 11:37:14 +0530

    Add files via upload

---
+++ date_format.rb
@@ -0,0 +1,30 @@
+require 'date' 
+class DateFormat 
+  def format()
+    puts "enter value for date"
+       @date = gets.chomp.to_s
+    puts "enter value for type"
+        @type = gets.chomp.to_s
+        
+        if @type == 'IST' 
+          puts Date.parse(@date).strftime("%d/%m/%Y") 
+          puts Date.parse(@date).strftime("%-d %B, %Y %Z") 
+          puts Date.parse(@date).strftime("%B %-d, %Y %Z") 
+          puts Date.parse(@date).strftime("%B %-d, %Y") 
+          puts Date.parse(@date).strftime("%Y-%m-%d")  
+          puts Date.parse(@date).strftime("%m/%d/%y")  
+        elsif @type == 'ORT' 
+          puts "conversion to ORT"
+          puts Date.parse(@date).strftime("%Y-%m-%d %H:%M:%S %z") 
+          puts Date.parse(@date).strftime("%Y-%m-%d %H:%M:%S %Z") 
+          puts Date.parse(@date).strftime("%Y/%m/%d %I:%M:%S %p") 
+          puts Date.parse(@date).strftime("%m/%d/%Y %I %p") 
+          puts Date.parse(@date).strftime("%Y/%m/%d %H:%M:%S") 
+        else @type == 'PST' 
+          puts "Default conversion to PST"
+          puts  Date.parse(@date).strftime("%m/%d/%Y")
+         end
+  end
+end
+obj = DateFormat.new;
+obj.format();
