c7b69d4eec7c8e330692c9e78669063c59d12ec9
MarlynKrishna <88415687+MarlynKrishna@users.noreply.github.com>
2022-07-26 17:33:32 +0530

    Add files via upload

---
+++ set_operations.rb
@@ -0,0 +1,61 @@
+class SetOperations
+    # @array1 = [ 2, 4, 5, 6, 7, 9, 1, 2, 4 ] 
+    # @array2 = [ 2, 4, 5, 6, 9, 8, 0, 3, 3 ] 
+    def init()
+        @array1 = []
+        @array2 = []
+        puts "enter value for n"
+        n = gets.chomp.to_i
+        puts "Enter values for array1:"
+        for i in 1..n
+            @array1 << gets.chomp.to_i 
+        end
+        puts "Enter values for array2:"
+        for j in 1..3
+            @array2 << gets.chomp.to_i
+        end
+        puts "Array1: #{@array1}"+"\n";
+        puts "Array2: #{@array2}";
+    end
+    def union
+        op = @array1 | @array2
+        puts "Result of Union Operation: #{op}";
+    end
+    def inter_section 
+        op = @array1 & @array2
+        puts "Result of InterSection Operation: #{op}";
+    end
+    def difference 
+        op = @array1 - @array2
+        puts "Results of Difference Operation: #{op}";
+    end
+    def a_compliment
+        puts "enter value of num1:"
+        @num1 = gets.chomp.to_i; 
+        op = ~@num1;
+        puts "Result of A's Compliment Operation: #{op}";
+    end
+    def cartesian_product()
+        @a= []
+        @b = []
+        puts "value of A:"
+        for i in 1..3
+            @a << gets.chomp.to_i
+        end
+        puts "value of B:"
+        for j in 1..2
+            @b << gets.chomp.to_i
+        end
+        op = (@a).product (@b);
+        puts "Result of Cartesian product result: #{op}";
+    end
+
+   
+end
+obj = SetOperations.new();
+obj.init();
+obj.union;
+obj.interSection;
+obj.difference;
+obj.a_compliment;
+obj.cartesian_product();
---
+++ student.rb
@@ -0,0 +1,35 @@
+class Student
+    def set_student_details()
+        print "StudentID:   ";
+        @roll_num = gets.strip
+        print "Student name: ";
+        @stud_name = gets.strip
+        print "Student Maths Marks: ";
+        @math_marks = gets.chomp.to_i
+        print "Student Arts Marks: ";
+        @arts_marks = gets.chomp.to_i
+        print "Student Bioolgy Marks: ";
+        @bio_marks = gets.chomp.to_i
+
+  
+        def total_marks()
+           @total =  @math_marks +  @arts_marks + @bio_marks;
+           
+        end
+        def display_stud_details
+            puts "\n"
+            puts "Details of the student:: "+"\n"
+            puts "Student Id: #{@rollNum}"
+            puts "Student Name:  #{@studName}"
+            puts "total marks of #{@studName}: #{@total}"
+        end
+
+    end
+
+
+end
+stu = Student.new();
+stu.set_student_details();
+stu.total_marks();
+stu.display_stud_details;
+
